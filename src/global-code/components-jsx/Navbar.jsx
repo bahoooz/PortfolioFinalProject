da8a67dda9901fe24639edec15c60de60efc703c
Bahoz <julienbraultcherif@gmail.com>
2024-01-30 19:09:46 +0100

    img navbar pour remonter en haut du site

--- src/global-code/components-jsx/Navbar.jsx
+++ src/global-code/components-jsx/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   Package,
   BookOpen,
@@ -12,34 +11,46 @@ const Navbar = () => {
   return (
     <nav>
       <div className="left-nav">
-        <img src={Logo} alt="logo bahoz" />
-        <a className="url" href="#">
+        <img
+          onClick={() => scrollTo({ top: "0", left: "0", behavior: "smooth" })}
+          src={Logo}
+          alt="logo bahoz"
+        />
+        <span className="url">
           bahoz-dev.netlify.app
-        </a>
+        </span>
       </div>
       <ul className="icons-navbar">
         <a href="#portfolio">
           <li>
             <Package weight="regular" />
-            <div className="infobulle-navbar"><span>Portfolio</span></div>
+            <div className="infobulle-navbar">
+              <span>Portfolio</span>
+            </div>
           </li>
         </a>
         <a href="#skills">
           <li>
             <Lightning weight="regular" />
-            <div className="infobulle-navbar"><span>Skills</span></div>
+            <div className="infobulle-navbar">
+              <span>Skills</span>
+            </div>
           </li>
         </a>
         <a href="#histoire">
           <li>
             <BookOpen weight="regular" />
-            <div className="infobulle-navbar"><span>Histoire</span></div>
+            <div className="infobulle-navbar">
+              <span>Histoire</span>
+            </div>
           </li>
         </a>
         <a href="#footer">
           <li>
             <EnvelopeSimpleOpen weight="regular" />
-            <div className="infobulle-navbar"><span>Contact</span></div>
+            <div className="infobulle-navbar">
+              <span>Contact</span>
+            </div>
           </li>
         </a>
       </ul>
